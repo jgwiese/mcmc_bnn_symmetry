59418cfaf173c6e51110104fed1b4332599688a3
Gregor Wiese <gregor.wiese@gmx.de>
2022-05-29 22:52:55 +0000

    updated globa_settings

--- src/global_settings.py
+++ src/global_settings.py
@@ -1,3 +1,8 @@
-# define paths
-PATH_DATASETS = "/home/gw/data/datasets/"
-PATH_RESULTS = "/home/gw/data/experiments/master_thesis/results"
+import os
+from pathlib import Path
+home = str(Path.home())
+
+
+PATH_DATASETS = os.path.join(home, "data/datasets/")
+PATH_RESULTS = os.path.join(home, "data/experiments/master_thesis/results")
+
